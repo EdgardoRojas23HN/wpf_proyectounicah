c4d4b055471d583ee96107f23ca8b323f698401c
Edgardo Rojas <erojas504@outlook.com>
2021-03-24 05:52:16 -0600

    actualizacion de la base de datos

--- database/reservaciones.sql
+++ database/reservaciones.sql
@@ -39,3 +39,55 @@ create table Habitaciones.Reservacion(
 		Primary key clustered (id)
 	)
 go
+
+create table Usuarios.Usuario(
+	id int not null identity (500, 1),
+	nombreCompleto varchar(255) not null,
+	username varchar(100) not null, 
+	password varchar(100) not null,
+	estado BIT not null,
+	constraint PK_Usuario_id
+	primary key clustered(id)
+	)
+go
+
+--restricciones de la tabla
+
+--el estado de las habitaciones es: ocupada, disponible, mantenimiento  y fuera de servicio
+ALTER TABLE Habitaciones.Habitacion With check
+add constraint CHK_Habitaciones_Habitacion$EstadoHabitaciones
+check (estado IN('OCUPADA', 'DISPONIBLE', 'MANTENIMIENTO', 'FUERADESERVICIO'))
+
+--LLAVE FORANEA PARA LAS HABOITACIONES
+ALter table Habitaciones.Reservacion
+	add constraint FK_Habitaciones_Reservacion$TieneUna$Habitaciones_Habitaciones
+	foreign key (habitacion) references Habitaciones.Habitacion(id)
+	on update no action
+	on delete no action
+go
+
+--La fecha de ingreso no puede ser menor que la fecha actual
+Alter table Habitaciones.Reservacion with check
+	add constraint CHK_Habitaciones_Habitacion$VerificarFechaIngreso
+	check (fechaIngreso >= GETDATE())
+GO
+
+--La fecha de salida no puede ser menor o igual ala fecha de ingreso
+alter table Habitaciones.Reservacion with check
+	add constraint CHK_Habitaciones_Habitacion$VerificarFechaSalida
+	check (fecha > fechaIngreso)
+go
+
+--No puede existir nombres de usuarios repetidos
+Alter Table Usuarios.Usuario
+	add constraint AK_Usuarios_Usuario_username
+	unique NONCLUSTERED (username)
+		
+go
+
+--la contraseña debe contener al menos 6 caracteres
+	ALTER TABLE Usuarios.Usuario with check
+	add constraint CHK_Usuarios_Usuario$VerificarLongitudContraseña
+	check (LEN(password) >=6 )
+go
+insert into Habitaciones.Habitacion VALUES (1, 'Habitacion sencilla', 10, 'NO DISPONIBLE') 
