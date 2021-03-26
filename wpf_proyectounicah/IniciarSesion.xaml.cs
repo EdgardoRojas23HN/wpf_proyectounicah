1ca6c21f18eebf62f73709ea3495c801be97ec69
Edgardo Rojas <erojas504@outlook.com>
2021-03-26 10:51:38 -0600

    Implementar la funcionalidad de inicio de sesion en el formulario

--- wpf_proyectounicah/IniciarSesion.xaml.cs
+++ wpf_proyectounicah/IniciarSesion.xaml.cs
@@ -18,11 +18,56 @@ namespace wpf_proyectounicah
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class IniciarSesion : Window
     {
-        public MainWindow()
+        //Objeto de tipo usuario para implementar su funcionalidad
+        private Usuario usuario = new Usuario();
+        public IniciarSesion()
         {
             InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Implementar la busqueda del usuario  desde la clase Usuario
+                Usuario elUsuario = usuario.BuscarUsuario(txtUsername.Text);
+
+                //Verficar si el usuario existe
+                if (elUsuario.Username == null)
+                {
+                    MessageBox.Show("El usuario o la contraseña son incorrectos. Favor verificar");
+                }
+                else
+                {
+                    //Verificar que la contraseña ingresada es igual  a la contraseña
+                    //almacenada en la base de datos
+
+                    if (elUsuario.Password == pwbPassword.Password)
+                    {
+                        MessageBox.Show("¡Bienvenido al sistema de reservaciones!");
+                    }
+                    else 
+                    {
+                        MessageBox.Show("El usuario o la contraseña son incorrectos. Favor verificar");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                //MessageBox.Show("Ha ocurrido un error la momento de realizar la consulta...");
+                MessageBox.Show(ex.Message);
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+        private void btnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
