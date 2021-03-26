1e410c91a6f6e897644e57ecd85e855be602a5d3
Edgardo Rojas <erojas504@outlook.com>
2021-03-26 12:54:20 -0600

    Creacion del formulario de menu principal  de la aplicacion

---
+++ wpf_proyectounicah/MenuPrincipal.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpf_proyectounicah
+{
+    /// <summary>
+    /// Lógica de interacción para MenuPrincipal.xaml
+    /// </summary>
+    public partial class MenuPrincipal : Window
+    {
+        public MenuPrincipal(string usuario)
+        {
+            InitializeComponent();
+            //Mostrar  el nombre del formulario
+            lblUsuario.Content = string.Format("Hola {0}! ¿Qué deseas realizar?", usuario);
+            
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            //Retornar el usuario al formulario de inicio de sesion
+            IniciarSesion iniciarSesion = new IniciarSesion();
+            iniciarSesion.Show();
+            Close();
+        
+        }
+
+    }
+}
